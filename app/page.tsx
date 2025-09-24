1c8c5e5c1675dd5f03b9e7e0ba6a886ad272838f
wdcs-karankadiya <karan@webcluesinfotech.com>
2025-09-24 17:52:02 +0530

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,23 @@
 "use client"; // 👈 this makes it a Client Component
 
 import { appUrl } from "./services/config";
+import { useSearchParams } from "next/navigation";
 
-const userId = "3758";
+// const userId = "3758";
 const baseUrl = appUrl;
 console.log(baseUrl);
-const imageUrl = `${baseUrl}/api/og?user_id=${userId}`;
 // const imageUrl = `https://twitter-og-beta.vercel.app/ETH_USD?side=BUY&leverage=1010&pnl=0&price=100`;
 
 export default function Home() {
+  const searchParams = useSearchParams();
+  const userId = searchParams.get("user_id");
+  console.log(userId, '======================================');
+  const imageUrl = `${baseUrl}/api/og?user_id=${userId}`;
+  const shareimageUrl = `${baseUrl}?user_id=${userId}`;
+
   const shareToTwitter = () => {
     const twitterUrl = `https://twitter.com/intent/tweet?text=Check out this image!&url=${encodeURIComponent(
-      imageUrl
+      shareimageUrl
     )}`;
     window.open(twitterUrl, "_blank");
   };
